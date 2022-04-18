753c977ad3007f42a8cbac557ba2b6dcb711e6c8
Pavezlo <Pavelzlo18@yandex.ru>
2022-04-18 03:19:14 +0300

    Добавьте файлы проекта.

---
+++ addressbook_tests_autoit/tests/GroupRemovalTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void GroupRemovalTest()
+        {
+            if (!app.Groups.GroupCheck())
+            {
+                GroupData group = new GroupData()
+                {
+                    Name = "delite"
+                };
+                app.Groups.Add(group);
+            }
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            app.Groups.Remove(0);
+            Assert.AreEqual(oldGroups.Count - 1, app.Groups.GetGroupCount());
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.RemoveAt(0);
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
